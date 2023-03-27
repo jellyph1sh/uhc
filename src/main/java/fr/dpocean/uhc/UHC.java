97a3de4541822108c4d6a5524ffeac3eca67fcb3
JellyPh1sh <sacha.sorgiati@gmail.com>
2023-03-27 04:11:43 +0200

    :rocket: | Setup base of the plugin.

---
+++ src/main/java/fr/dpocean/uhc/UHC.java
@@ -0,0 +1,12 @@
+package fr.dpocean.uhc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class UHC extends JavaPlugin {
+    
+    @Override
+    public void onEnable() {
+        
+    }
+
+}
